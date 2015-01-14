c6d830df2a6d23d35bb82af79ad7e2524f28a7a0
MichalKs <xiezopolskim@gmail.com>
2015-01-14 21:49:04 +0100

    Compilation OK!!!

---
+++ STM32F4_ChibiOS/main.c
@@ -0,0 +1,21 @@
+/**
+ * @file    main.c
+ * @brief
+ * @date    14 sty 2015
+ * @author  Michal Ksiezopolski
+ *
+ *
+ * @verbatim
+ * Copyright (c) 2014 Michal Ksiezopolski.
+ * All rights reserved. This program and the
+ * accompanying materials are made available
+ * under the terms of the GNU Public License
+ * v3.0 which accompanies this distribution,
+ * and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * @endverbatim
+ */
+
+int main() {
+
+}
