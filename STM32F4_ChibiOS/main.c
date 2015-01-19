df83fd545a2638e641cae1d953694547c595be18
MichalKs <xiezopolskim@gmail.com>
2015-01-19 21:43:21 +0100

    Blue LED blinking happily away

--- STM32F4_ChibiOS/main.c
+++ STM32F4_ChibiOS/main.c
@@ -16,6 +16,20 @@
  * @endverbatim
  */
 
+#include "ch.h"
+#include "hal.h"
+
 int main() {
+
+  halInit();
+  chSysInit();
+  palSetPadMode(GPIOD, GPIOD_LED6, PAL_MODE_OUTPUT_PUSHPULL);
+
+  while (TRUE) {
+    palSetPad(GPIOD, GPIOD_LED6); /* Blue LED */
+    chThdSleepMilliseconds(500);
+    palClearPad(GPIOD, GPIOD_LED6);
+    chThdSleepMilliseconds(500);
+  }
 
 }
